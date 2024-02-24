3265300d3bf74266cdcc0e85a968c15e271da0e2
Houssam2001 <houssam2001mchmch@gmail.com>
2024-02-24 16:52:00 +0100

    Initial commit

---
+++ src/main/java/com/microservice/inventoryservice/entities/Product.java
@@ -0,0 +1,2 @@
+package com.microservice.inventoryservice.entities;public class Product {
+}
---
+++ src/main/java/com/microservice/inventoryservice/repositories/ProductRepository.java
@@ -0,0 +1,2 @@
+package com.microservice.inventoryservice.repositories;public class ProductRepository {
+}
